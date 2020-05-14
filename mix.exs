3429f6b6dd937cb2751ef5cf5dc00b3463b415d6
Patrick Smith <patrick@burntcaramel.com>
2020-05-14 22:17:21 +1000

    Add ex_doc dep

--- mix.exs
+++ mix.exs
@@ -24,8 +24,8 @@ defmodule Paredown.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:phoenix_html, "~> 2.14"}
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 
